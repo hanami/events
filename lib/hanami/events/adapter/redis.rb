b0f77e1facbc9336fc3ab79db235ed8855342f70
Anton Davydov <antondavydov.o@gmail.com>
2017-08-01 01:05:38 +0300

    Use only one redis subscriber

--- lib/hanami/events/adapter/redis.rb
+++ lib/hanami/events/adapter/redis.rb
@@ -4,26 +4,50 @@ module Hanami
   module Events
     class Adapter
       class Redis
+        EVENTS_NAME = 'hanami_event'
+
         attr_reader :listeners
 
         def initialize(redis:)
           @redis = redis
+          @listeners = []
+          @uniq_thread = false
         end
 
         def announce(event_name, payload)
-          @redis.publish("hanami-events.#{event_name}", payload.to_json)
+          @redis.publish(EVENTS_NAME, payload.to_json)
         end
 
         def subscribe_pattern(event_name, &block)
-          Thread.new do
-            listener = Listener.new(event_name, block)
+          @listeners << Listener.new(event_name, block)
 
+          return if thread_spawned?
+          thread_spawned!
+
+          Thread.new do
             @redis.with do |connection|
-              connection.subscribe("hanami-events.#{event_name}") do |on|
-                on.message { |channel, msg| listener.call(event_name, JSON.parse(msg)) }
+              connection.subscribe(EVENTS_NAME) do |on|
+                on.message { |_, message| call_listeners(event_name, message)  }
               end
             end
           end
+        end
+
+        private
+
+        def thread_spawned?
+          @thread_spawned
+        end
+
+        def thread_spawned!
+          @thread_spawned = true
+        end
+
+        def subscribe_to_events(connection)
+        end
+
+        def call_listeners(event_name, message)
+          @listeners.each { |listener| listener.call(event_name, JSON.parse(message)) }
         end
       end
     end
