d00303dd3ef4e109894c364acad28bb944845443
Anton Davydov <antondavydov.o@gmail.com>
2017-08-04 00:27:42 +0300

    Fix adapter specs

--- spec/unit/hanami/events/adapter_spec.rb
+++ spec/unit/hanami/events/adapter_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe Hanami::Events::Adapter do
   describe '.keys' do
-    it { expect(described_class.keys).to eq %w[memory redis] }
+    it { expect(described_class.keys).to match %w[memory redis] }
   end
 
   describe '.[]' do
