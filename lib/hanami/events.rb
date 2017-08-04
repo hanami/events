45bf562f07ee51f5929cf0cac21fa7e42ca6e5b5
Anton Davydov <antondavydov.o@gmail.com>
2017-08-04 11:04:56 +0300

    Add simple documentation for Hanami::Events module

--- lib/hanami/events.rb
+++ lib/hanami/events.rb
@@ -4,8 +4,28 @@ require "hanami/events/base"
 require "hanami/events/subscriber"
 require "hanami/events/version"
 
+# Events framework for Hanami
+#
+# @since x.x.x
+#
+# @see https://github.com/hanami/events
 module Hanami
   module Events
+    # Build event instance with selected adapter
+    #
+    # @param adapter_name [Symbol] the adapter type
+    # @param options [Hash] the configuration hash
+    #
+    # @since x.x.x
+    #
+    # @example memory adapter
+    #
+    #   Hanami::Events.build(:memory)
+    #
+    # @example redis adapter
+    #
+    #   redis = ConnectionPool.new(size: 5, timeout: 5) { Redis.new(host: 'localhost', port: 6379) }
+    #   Hanami::Events.build(:redis, redis: redis)
     def self.build(adapter_name, **options)
       Base.new(adapter_name, options)
     end
