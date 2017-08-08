f3357d8f8dbb48515c23ebe31f3c0463e9b3bda0
Anton Davydov <antondavydov.o@gmail.com>
2017-08-09 00:46:33 +0300

    Add documentation for Subscriber class

--- lib/hanami/events/subscriber.rb
+++ lib/hanami/events/subscriber.rb
@@ -1,6 +1,14 @@
 module Hanami
   module Events
+    # Subscriber class for calling subscriber blocks
+    #
+    # @since x.x.x
+    #
+    # @api private
     class Subscriber
+      # @since x.x.x
+      #
+      # @api private
       class Runner
         attr_reader :logger
 
@@ -17,13 +25,26 @@ module Hanami
       def initialize(event_name, event_handler, logger = nil)
         @event_name = event_name
         @pattern_matcher = Matcher.new(event_name)
-        @handler_runner = Runner.new(event_handler, logger)
+        @runner = Runner.new(event_handler, logger)
       end
 
+      # Call runner with payload if event match by subscribe pattern
+      #
+      # @param event_name [String] the event name
+      # @param payload [Hash] the payload
+      #
+      # @since x.x.x
+      #
+      # @api private
       def call(event_name, payload)
-        @handler_runner.(payload) if @pattern_matcher.match?(event_name)
+        @runner.(payload) if @pattern_matcher.match?(event_name)
       end
 
+      # Returns meta information for subscriber
+      #
+      # @since x.x.x
+      #
+      # @api private
       def meta
         { name: @event_name }
       end
--- lib/hanami/events/version.rb
+++ lib/hanami/events/version.rb
@@ -1,5 +1,7 @@
 module Hanami
   module Events
+    # @since x.x.x
+    # @api private
     VERSION = "0.0.0".freeze
   end
 end
