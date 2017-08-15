50df079d402ad4e3eb2fdf93779f57a05fc89611
Anton Davydov <antondavydov.o@gmail.com>
2017-08-16 00:44:19 +0300

    Fix version spec

--- spec/unit/hanami/events/version_spec.rb
+++ spec/unit/hanami/events/version_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe "Hanami::Events::VERSION" do
   it 'exposes version' do
-    expect(Hanami::Events::VERSION).to eq("0.0.0")
+    expect(Hanami::Events::VERSION).to eq('0.1.0')
   end
 end
