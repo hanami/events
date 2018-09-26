aac43f08a1ee501f0c4c995b71cf9b0c81ed6f3e
Anton Davydov <antondavydov.o@gmail.com>
2018-09-27 02:21:48 +0300

    Update memory example

--- examples/memory.rb
+++ examples/memory.rb
@@ -1,11 +1,16 @@
 require 'hanami/events'
 
-events = Hanami::Events.initialize(:memory_sync)
+events = Hanami::Events.initialize(:memory_async, logger: Logger.new(STDOUT))
 
-events.subscribe('user.created') { |payload| puts "Create user: #{payload}" }
-events.subscribe('user.created') { |payload| puts "Send notification to user: #{payload}" }
+events.subscribe('user.created') { |payload| logger.info "Create user: #{payload}" }
+events.subscribe('user.created') { |payload| logger.info "Send notification to user: #{payload}" }
 
-events.subscribe('user.deleted') { |payload| puts "Delete user: #{payload}" }
+events.subscribe('user.deleted') { |payload| logger.info "Delete user: #{payload}" }
+
+runner = Hanami::Events::Runner.new(events)
+Thread.new { runner.start }
 
 events.broadcast('user.created', user_id: 1)
 events.broadcast('user.deleted', user_id: 1)
+
+sleep 1
